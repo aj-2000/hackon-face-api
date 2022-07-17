b038aab8a4a3c10ab154b6d106831da0094585b2
adi-coder2000 <contact.quicktech@gmail.com>
2022-07-17 06:39:21 +0530

    CHECKPOINT: Before PyMongo

--- App/models.py
+++ App/models.py
@@ -1,3 +1,19 @@
-from django.db import models
 
-# Create your models here.
+from djongo import models
+from djongo.models.indexes import Co
+
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=1024)
+    full_name = models.CharField(max_length=255)
+    img_url = models.URLField()
+    encoding = models.CharField(max_length=5000)
+
+    class Meta:
+        indexes = [
+            TextIndex(fields=['username', 'password'])
+        ]
+    
+
+
